8367f96d1809d2da4f45fb652302f5dcf7c3382e
AwakexD <aleks.parvanow@gmail.com>
2023-07-15 13:47:31 +0300

    100/100 H1:M28

--- JS Advanced/Objects and Composition/demo.js
+++ JS Advanced/Objects and Composition/demo.js
@@ -7,3 +7,29 @@ const person = {
 
 console.log(person.isAdult()); 
 
+// Destructoring Assingment Syntax
+
+let {name, age} = person;
+console.log(name, age); 
+
+// Clone Object
+let {...clonedPerson} = person;
+clonedPerson.name = "Pesho";
+console.log(clonedPerson);
+
+// Comparing objects
+let person1 = {name : 'Pesho'}
+let person2 = {name : 'Pesho'}
+console.log(person1 == person2);
+
+// Associative Array
+let phones = {
+    'Ivan Petrov': '783921321231',
+    'Georgi Georgiev': '812341783924',
+    'Petar Petrov': '123543653734', 
+};
+
+// Iteration 
+for (key in phones) {
+    console.log(`${key} - ${phones[key]}`);
+}
