cd47d5db57b5f04a0e8e7040ca884ccf1a6ca624
AwakexD <aleks.parvanow@gmail.com>
2023-07-27 16:30:16 +0300

    Exercise02

--- JS Advanced/DOM Manipulations and Events/Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulations and Events/Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInputElement = document.querySelector('input[name="email"]').value;
+    let emailCellElement = document.querySelectorAll('tr td:nth-of-type(2)');
+    let resultElement = document.getElementById('result');
+
+    let targetElelement = Array.from(emailCellElement).find(x => x.textContent === emailInputElement);
+
+    if (targetElelement) {
+        targetElelement.parentNode.remove()
+        resultElement.textContent = 'Deleted.';
+
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
 }
