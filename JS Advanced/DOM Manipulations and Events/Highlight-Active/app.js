b32cf9d4c4469104d6a7c7ceb1d67273786027e7
AwakexD <aleks.parvanow@gmail.com>
2023-07-30 23:15:34 +0300

    Commit changes

--- JS Advanced/DOM Manipulations and Events/Highlight-Active/app.js
+++ JS Advanced/DOM Manipulations and Events/Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    let sectionsElement = document.querySelectorAll('input[type="text"]');
+    console.log(sectionsElement);
+
+    sectionsElement.forEach(element => {
+        element.addEventListener('focus', (e) => {
+            e.target.parentElement.classList.add('focused');
+        });
+
+        element.addEventListener('blur', (e) => {
+            e.target.parentElement.classList.remove('focused');
+        })
+    });
 }
