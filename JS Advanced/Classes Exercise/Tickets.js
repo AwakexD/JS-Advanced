f9149d111259e4ebaaf297db7cd203625df67d00
AwakexD <aleks.parvanow@gmail.com>
2023-08-22 23:38:04 +0300

    Commit changes

--- JS Advanced/Classes Exercise/Tickets.js
+++ JS Advanced/Classes Exercise/Tickets.js
@@ -24,3 +24,10 @@ solution(['Philadelphia|94.20|available',
 'New York City|95.99|sold',
 'Boston|126.20|departed'],
 'destination');
+
+solution(['Philadelphia|94.20|available',
+'New York City|95.99|available',
+'New York City|95.99|sold',
+'Boston|126.20|departed'],
+'status'
+)
